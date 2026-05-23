a86943676d084a6fca5ce0f36928193273c2920c
rajib777durjoy <rajib777chanda@gmail.com>
2026-05-23 23:37:50 +0600

    install multer for file handle

--- app/socket/socket.js
+++ app/socket/socket.js
@@ -1,20 +1,21 @@
-
-import sql from "@/lib/db.connect";
 import { Server } from "socket.io";
+
 const io = new Server(3001, {
-    cors: {
-        origin: "http://localhost:3000",
-    },
+  cors: {
+    origin: ["http://localhost:3000"],
+  },
 });
 
 io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
-    // add socket id in users-table //
-    socket.on('current_user',(data)=>{
-     const id = data.user_id;
-    })
-    
-    socket.on("disconnect", () => {
-        console.log("Disconnected:", socket.id);
-    });
+  console.log("User connected:", socket.id);
+
+  socket.on("current_user", (data) => {
+    console.log("User ID:", data.user_id);
+
+    // example: save socket.id with user_id
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected:", socket.id);
+  });
 });
